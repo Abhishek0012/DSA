2d317d37a67117bebd932c0e313e011bedd7af96
Abhishek0012 <71779748+Abhishek0012@users.noreply.github.com>
2022-01-17 05:58:04 +0530

    commit

--- segmentree.cpp
+++ segmentree.cpp
@@ -1,63 +1,75 @@
 #include<bits/stdc++.h>
 using namespace std;
-class segment
+class segment_tree
 {
     public:
-    vector<int> seg;
     vector<int> arr;
-    segment(int n)
+    vector<int> seg;
+    segment_tree(int n)
     {
-        seg.resize(4*n,0);
-        arr.resize(n+1,0);
+        arr.resize(n+1);
+        seg.resize(4*n);
     }
     void build(int src , int l , int r)
     {
-        if(l > r ) return ;
-        if(l==r ) 
+        if(l==r)
         {
             seg[src]=arr[l];
+            cout<<seg[src]<<" "<<l<<" "<<r<<endl;
             return;
         }
         int mid = l + (r-l)/2;
-        build(src*2,l,mid);
-        build(src*2+1 , mid+1 , r);
+        build(2*src , l , mid);
+        build(2*src+1 , mid+1 ,r);
+        seg[src]=seg[src*2]+seg[src*2+1];
+        cout<<seg[src]<<" "<<l<<" "<<r<<endl;
+    }
+    void update(int src , int l , int r , int val , int ind)
+    {
+        if(ind==l && l==r)
+        {
+            arr[ind]=val;
+            seg[src]=val;
+            return;
+        }
+        int mid = l+(l-r)/2;
+        if(ind <=mid) update(src*2,l,mid,val,ind);
+        else update(src*2+1,mid+1,r,val,ind);
         seg[src]=seg[src*2]+seg[src*2+1];
     }
-    int query(int src , int x , int y , int l , int r)
+    int query(int src , int l ,int r  , int x , int y)
     {
-        if(y < l || x  > r || l > r) return 0;
+        if(x>r || y<l || l>r) return 0;
         if(x<=l && y>=r) return seg[src];
         int mid = l + (r-l)/2;
-        int a = query(src*2 , x , y , l , mid);
-        int b = query(src*2+1 , x, y , mid + 1 , r);
-        seg[src]=a+b;
+        return query(src*2,l,mid,x,y) + query(src*2+1,mid+1,r,x,y);
     }
-    void update(int src  , int x , int l , int r)
-    {
-        if(l > r) return ;
-        if(l==r && l==x)
-        {
-            seg[src]=x;
-            arr[x]=x;
-            return;
-        }
-        int mid = l + (r-l)/2;
-        if(x <=mid) update(src*2 , x , l , mid);
-        else update(src*2+1 , x , mid + 1 , r);
-        seg[src]=seg[src*2]+seg[src*2+1];
-    }
-
 };
-int main()
+void solve()
 {
-    int n ;
+    int n;
     cin>>n;
-    segment obj(n);
+    segment_tree obj(n);
     for(int i=1;i<=n;i++)
     {
         cin>>obj.arr[i];
     }
     obj.build(1,1,n);
-    int x = obj.query(1,2,4,1,n);
-    cout<<x<<endl;
+    int q;
+    cin>>q;
+    for(int i=0;i<q;i++)
+    {
+        int x,y;
+        cin>>x>>y;
+        cout<<obj.query(1,1,n,x,y)<<endl;
+    }
+}
+int main()
+{
+    int test;
+    cin>>test;
+    while(test--)
+    {
+        solve();
+    }
 }
